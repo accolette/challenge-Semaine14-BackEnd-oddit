f42215608e788a4c8c50c79c413815d05e7b7c61
Aurore Collet <auroreclt@gmail.com>
2026-03-06 11:56:35 +0100

    fix #15 création de compte OK avec hash du mdp

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,10 @@
+import argon2 from "argon2";
+
 import { AppUser } from "../models/index.js";
 
-export async function registrate(req, res, next) {
+export async function registerUser(req, res, next) {
+  req.body.password = await argon2.hash(req.body.password);
+
   try {
     const user = await AppUser.create(req.body);
     res.status(201).json({ "New user created : ": user });
